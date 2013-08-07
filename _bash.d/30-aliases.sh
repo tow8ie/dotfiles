c1cfcdf3543203db4703b91e539f442175663c57
Tobias Adam <tow8ie@gmail.com>
2013-08-07 10:23:40 +0200

    Only call rbenv if rbenv is present.

--- _bash.d/30-aliases.sh
+++ _bash.d/30-aliases.sh
@@ -123,13 +123,15 @@ alias t='tmux'
 # hitch #
 # ----- #
 
-if `rbenv which hitch 2> /dev/null`; then
-  hitch() {
-    command hitch "$@"
-    if [[ -s "$HOME/.hitch_export_authors" ]] ; then source "$HOME/.hitch_export_authors" ; fi
-  }
-  alias unhitch='hitch -u'
-  alias hitched='echo $GIT_AUTHOR_NAME'
-  hitch # Persist pair info between terminal instances
+if `which rbenv 2> /dev/null`; then
+  if `rbenv which hitch 2> /dev/null`; then
+    hitch() {
+      command hitch "$@"
+      if [[ -s "$HOME/.hitch_export_authors" ]] ; then source "$HOME/.hitch_export_authors" ; fi
+    }
+    alias unhitch='hitch -u'
+    alias hitched='echo $GIT_AUTHOR_NAME'
+    hitch # Persist pair info between terminal instances
+  fi
 fi
 
