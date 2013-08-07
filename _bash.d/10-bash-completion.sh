04ded20bfa239620230905c2d3c1cea1dbd71d7a
Tobias Adam <tow8ie@gmail.com>
2013-08-07 10:28:02 +0200

    Only call brew if brew is installed.

--- _bash.d/10-bash-completion.sh
+++ _bash.d/10-bash-completion.sh
@@ -1,6 +1,8 @@
 # bash-completion: http://bash-completion.alioth.debian.org/
 # Installed via Homebrew
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-  . `brew --prefix`/etc/bash_completion
+if `which brew 2> /dev/null`; then
+  if [[ -f `brew --prefix`/etc/bash_completion ]]; then
+    . `brew --prefix`/etc/bash_completion
+  fi
 fi
 
