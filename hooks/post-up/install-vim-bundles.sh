cf85063ee865a2dcc6de7d795d7dcb337990f399
Tobias Adam <mail@tobiadam.de>
2017-06-27 17:42:28 +0200

    Disable rcm post hook for auto-installing vim bundles
    It does not really work at the moment (the terminal is broken after the
    script is run; every command ends with an error) and I need some time to
    look into it.

--- hooks/post-up/install-vim-bundles.sh
+++ hooks/post-up/install-vim-bundles.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-if [ ! -e $HOME/.vim/bundle/vundle ]; then
-  echo "Installing Vundle"
-  git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-fi
+scriptname=`basename "$0"`
 
-echo "Installing Vundle bundles"
-vim -u $HOME/.vimrc.bundles +PluginInstall +qa
+echo ""
+echo "Message from '$scriptname':"
+echo "The rcm post hook for auto-installing vim bundles does not work at the moment and should be fixed. :-("
+
+# if [ ! -e $HOME/.vim/bundle/vundle ]; then
+#   echo "Installing Vundle"
+#   git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+# fi
+#
+# echo "Installing Vundle bundles"
+# vim -u $HOME/.vimrc.bundles +PluginInstall +qa
 
