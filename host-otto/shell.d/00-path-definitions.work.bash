5ef88736a24207690dac7686913740ef27d2ef5d
Tobias Adam <mail@tobiadam.de>
2018-07-10 15:26:32 +0200

    Create rcm host for otto.

---
+++ host-otto/shell.d/00-path-definitions.work.bash
@@ -0,0 +1,22 @@
+# These path definitions will be _prepended_ to $PATH.
+# Order dependent, most important first.
+pre_path_definitions=(
+  # ./bin                     # Bundler binstubs
+  $HOME/.bin                # dotfiles bins
+  $HOME/bin                 # Personal bins
+  # /usr/local/share/npm/bin  # npm global bins
+  /usr/local/bin            # Homebrew bins
+  $HOME/.cask/bin           # Cask bins
+  # /opt/local/bin            # Macport bins
+  # $HOME/dart/dart-sdk/bin   # Dart bins
+  # $HOME/bin/vmware          # Vmware bins
+  # $HOME/Library/Haskell/bin # cabal (Haskell)
+)
+
+# These path definitions will be _appended_ to $PATH.
+# Order dependent, most important first.
+post_path_definitions=(
+  # ./node_modules/.bin       # npm local bins
+  # `yarn global bin`         # yarn
+)
+
