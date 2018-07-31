a6ea09e32cde3067682d9e6fae0fba62117812bb
Tobias Adam <mail@tobiadam.de>
2018-07-31 23:01:04 +0200

    Fix termial being broken after plugins are installed.

--- hooks/post-up/install-vim-bundles.sh
+++ hooks/post-up/install-vim-bundles.sh
@@ -12,4 +12,7 @@ fi
 
 echo "Installing Vundle bundles"
 vim -u $HOME/.vimrc.bundles +PluginInstall +qall
+# Without this call the termial is broken after the Vim Plugin call above.
+# Taken from: https://github.com/thoughtbot/dotfiles/blob/d6014c3a606df5ff589d864b1f23cb53284a612e/hooks/post-up#L13
+reset -Q
 
