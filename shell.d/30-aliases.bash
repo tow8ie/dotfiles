c6067a61382314f0c261da1b08a83cf0fb0f699c
Tobias Adam <mail@tobiadam.de>
2018-08-01 10:54:23 +0200

    Add some handy bash aliases for calculating file checksums.

--- shell.d/30-aliases.bash
+++ shell.d/30-aliases.bash
@@ -150,3 +150,16 @@ alias r='ranger'
 
 alias gdl='./gradlew'
 
+# --------- #
+# Checksums #
+# --------- #
+
+alias sha256='shasum -a 256'
+alias sha1='openssl sha1'
+
+# How to check if a program exists from a Bash script?
+# https://stackoverflow.com/a/26759734
+if ! [ -x "$(command -v md5)" ]; then
+  alias md5='openssl md5'
+fi
+
