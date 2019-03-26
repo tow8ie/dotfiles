1df35b95dc6692314aa38e1d79edfc4b937199ca
Tobias Adam <mail@tobiadam.de>
2019-03-26 12:03:36 +0100

    Set LANG env var to English (so, for example, Git does not speak German).

--- shell.d/02-env.sh
+++ shell.d/02-env.sh
@@ -7,3 +7,7 @@ export TERM="screen-256color"
 # Don’t let Java speak German
 export JAVA_TOOL_OPTIONS=-Duser.language=en
 
+# Git started to speak German. English seems to be the better default
+# nonetheless.
+export LANG=en_US.UTF-8
+
