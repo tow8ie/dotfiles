7290b7ea2d2b9f0f84e5edf5fd50231ab73ee9e9
Tobias Adam <mail@tobiadam.de>
2019-09-23 11:22:22 +0200

    Make lein/profiles.clj host specific.

---
+++ host-home/lein/profiles.clj
@@ -0,0 +1,2 @@
+{:user {:plugins [[lein-pprint "1.2.0"]
+                  [lein-ancient "0.6.15"]]}}
---
+++ host-otto/lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [;; Does not work for JDK 12 + Clojure 1.10.0 + lein 2.9.1
+                  ;; @see: https://github.com/venantius/ultra/issues/108
+                  #_[venantius/ultra "0.6.0"]]}}
+
