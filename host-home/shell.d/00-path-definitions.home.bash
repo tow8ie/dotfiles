c825823a7feb77127499397c6a4e14d239446601
Tobias Adam <mail@tobiadam.de>
2021-03-04 01:17:27 +0100

    Simplify PATH calculation
    - Enhance PATH with Visual Studio Code bin path
    - The Bash "function" that deduplicated PATH entries did not work with
      paths that contained spaces. Removed this function because it's just
      not worth it to do "complex" things with Bash.
    - Also simplified Bash code that appends and prepends to PATH.

--- host-home/shell.d/00-path-definitions.home.bash
+++ host-home/shell.d/00-path-definitions.home.bash
@@ -1,17 +1,41 @@
 # These path definitions will be _prepended_ to $PATH.
 # Order dependent, most important first.
+
 pre_path_definitions=(
-  bin                                                  # Bundler binstubs
-  $HOME/.bin                                           # dotfiles bins
-  $HOME/bin                                            # Personal bins
-  /Applications/Postgres.app/Contents/Versions/9.3/bin # Postgres.app bins
-  /usr/local/bin                                       # Homebrew bins
-  /usr/local/sbin                                      # Homebrew bins
+
+  # Bundler binstubs
+  bin
+
+  # dotfiles bins
+  $HOME/.bin
+
+  # Visual Studio Code
+  "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+  # Personal bins
+  $HOME/bin
+
+  # Postgres.app bins
+  /Applications/Postgres.app/Contents/Versions/9.3/bin
+
+  # Homebrew bins
+  /usr/local/bin
+
+  # Homebrew bins
+  /usr/local/sbin
+
+  # Rust bins
+  $HOME/.cargo/bin
+
 )
 
 # These path definitions will be _appended_ to $PATH.
 # Order dependent, most important first.
+
 post_path_definitions=(
-  ./node_modules/.bin       # npm local bins
+
+  # npm local bins
+  ./node_modules/.bin
+
 )
 
--- shell.d/01-path-helpers.bash
+++ shell.d/01-path-helpers.bash
@@ -6,62 +6,22 @@
 # for this script to work. They are located in the host specific
 # 00-path-definitions.*.sh file
 
-glu() {
-
-# Joins strings on separate lines to one string with the character given as
-# first argument.
-
-  local IFS="$1"
-  shift && echo "$*"
-}
-
-pre_paths() {
-  # Converts $pre_path_definitions array to space separated string
-  local _paths=`echo "${pre_path_definitions[@]/#/ }"`
-  # Replaces spaces of string with colons
-  glu ":" $_paths
-}
-
-post_paths() {
-  # Converts $post_path_definitions array to space separated string
-  local _paths=`echo "${post_path_definitions[@]/#/ }"`
-  # Replaces spaces of string with colons
-  glu ":" $_paths
-}
-
-repath() {
-
-# Removes duplicate entries in $PATH (keeps the one that occurs the first
-# time).
-# Found at:
-# http://www.commandlinefu.com/commands/view/8606/speed-up-builds-and-scripts-remove-duplicate-entries-in-path.-users-scripts-are-oftern-bad-pathapathpath-type-of-thing-cause-diplicate.
-#
-# Explanation of crucial part, found at:
-# http://www.tek-tips.com/viewthread.cfm?qid=1585675
-#
-# x[$0]++ increments the value of a hash member indexed by the contents of the
-# current line. So the first time a line is encountered, it will be changed
-# from "undefined" (or 0 as far as awk is concerned) to 1. The second time a
-# matching line is encountered, it will be increased to 2, etc. The ++ being
-# appended to the variable means the increment occurs after the expression is
-# evaluated. If it was ++x[$0] it would happen before the expression was
-# evaluated. You'll notice there is no code in { brackets } after the
-# expression. This means that awk will take the default action of printing the
-# input line when the expression is "true". By placing ! in front of the
-# expression, it means that the overall result is "true" when the contents of
-# the hash are undefined (i.e. 0), or "false" when it contains any other value
-# (i.e. an identical line has been encountered previously), and in the latter
-# case, nothing is printed.
-
-  _E=`echo "${PATH//:/$'\n'}" | awk '!x[$0]++'`
-  glu ":" $_E
-}
-
 debug_path() {
   for i in $(echo $PATH | tr ":" "\n"); do echo $i; done
 }
 
-PATH="`pre_paths`:$PATH:`post_paths`"
+# https://unix.stackexchange.com/a/634670
+pathedit() {
+    [ -z "$2" ] && return 2
+    PATH=$(printf ":$PATH:" | sed "s:\:$2\::\::g")
+    case $1 in
+    -p ) PATH=$2$PATH ;;
+    -a ) PATH=$PATH$2 ;;
+    -r ) ;;
+    * ) return 2 ;;
+    esac
+    PATH=$(printf "$PATH" | tr -s :) PATH=${PATH#:} PATH=${PATH%:}
+}
 
 # asdf version manager (for Node.js)
 
@@ -90,9 +50,20 @@ fi
 
 # Add the bin subdirectory of the GOPATH to the PATH
 if command -v go 1>/dev/null 2>&1; then
-  PATH="${PATH}:$(go env GOPATH)/bin"
+  pathedit -a "$(go env GOPATH)/bin"
 fi
 
-PATH=`repath`
+# Prepend paths
+
+for (( idx=${#pre_path_definitions[@]}-1 ; idx>=0 ; idx-- )) ; do
+    pathedit -p "${pre_path_definitions[idx]}"
+done
+
+# Append paths
+
+for (( idx=${#post_path_definitions[@]}-1 ; idx>=0 ; idx-- )) ; do
+    pathedit -a "${post_path_definitions[idx]}"
+done
+
 export PATH
 
