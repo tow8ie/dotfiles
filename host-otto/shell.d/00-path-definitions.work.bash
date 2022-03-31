b2571dce1422f254bc2d5b93f23f6ff2fc693dcd
Tobias Adam <mail@tobiadam.de>
2022-03-31 18:39:30 +0200

    Update PATH definitions

--- host-otto/shell.d/00-path-definitions.work.bash
+++ host-otto/shell.d/00-path-definitions.work.bash
@@ -1,24 +1,62 @@
 # These path definitions will be _prepended_ to $PATH.
 # Order dependent, most important first.
+
 pre_path_definitions=(
-  # ./bin                            # Bundler binstubs
-  $HOME/.bin                         # dotfiles bins
-  $HOME/bin                          # Personal bins
-  # /usr/local/share/npm/bin         # npm global bins
-  /usr/local/bin                     # Homebrew bins
-  /usr/local/sbin                    # Homebrew bins
-  $HOME/.cask/bin                    # Cask bins
-  # /opt/local/bin                   # Macport bins
-  # $HOME/dart/dart-sdk/bin          # Dart bins
-  # $HOME/bin/vmware                 # Vmware bins
-  # $HOME/Library/Haskell/bin        # cabal (Haskell)
+
+  # Bundler binstubs
+  # ./bin
+
+  # dotfiles bins
+  $HOME/.bin
+
+  # Visual Studio Code
+  "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+  # Personal bins
+  $HOME/bin
+
+  # npm global bins
+  # /usr/local/share/npm/bin
+
+  # Homebrew bins
+  /usr/local/bin
+
+  # Homebrew bins
+  /usr/local/sbin
+
+  # Cask bins
+  $HOME/.cask/bin
+
+  # Macport bins
+  # /opt/local/bin
+
+  # Dart bins
+  # $HOME/dart/dart-sdk/bin
+
+  # Vmware bins
+  # $HOME/bin/vmware
+
+  # cabal (Haskell)
+  # $HOME/Library/Haskell/bin
+
 )
 
 # These path definitions will be _appended_ to $PATH.
 # Order dependent, most important first.
+
 post_path_definitions=(
-  /usr/local/opt/python/libexec/bin/ # Unversioned symlinks for Python installed by Homebrew
-  # ./node_modules/.bin              # npm local bins
-  # `yarn global bin`                # yarn
+
+  # Unversioned symlinks for Python installed by Homebrew
+  /usr/local/opt/python/libexec/bin/
+
+  # pipx
+  $HOME/.local/bin
+
+  # npm local bins
+  # ./node_modules/.bin
+
+  # yarn
+  # `yarn global bin`
+
 )
 
