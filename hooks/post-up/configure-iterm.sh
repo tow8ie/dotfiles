40c2c8f75a69405c3e1ac033a956f87175fdde63
Tobias Adam <mail@tobiadam.de>
2023-01-27 17:37:28 +0100

    Disable iTerm preferences install hook.

--- hooks/post-up/configure-iterm.sh
+++ hooks/post-up/configure-iterm.sh
@@ -3,11 +3,11 @@
 # Taken from: https://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
 # Does not work too well, might need some additional manual setup.
 
-if [[ "$(uname)" == "Darwin" && -d "/Applications/iTerm.app" ]]; then
-  echo "Configuring iTerm."
-  # Specify the preferences directory
-  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/.dotfiles/iterm2"
-  # Tell iTerm2 to use the custom preferences in the directory
-  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-fi
+# if [[ "$(uname)" == "Darwin" && -d "/Applications/iTerm.app" ]]; then
+#   echo "Configuring iTerm."
+#   # Specify the preferences directory
+#   defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${HOME}/.dotfiles/iterm2"
+#   # Tell iTerm2 to use the custom preferences in the directory
+#   defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+# fi
 
