12fae815b484e9b2145d45ddadd1a61c1df97341
Tobias Adam <tow8ie@users.noreply.github.com>
2023-01-30 18:12:52 +0100

    Slim down default zsh PATH.

--- shell.d/00-path-definitions.zsh
+++ shell.d/00-path-definitions.zsh
@@ -9,17 +9,14 @@ typeset -gx -U path
 
 path=(
 
-  bin                                                  # Bundler binstubs
+  # bin                                                # Bundler binstubs
   $HOME/.bin                                           # dotfiles bins
   $HOME/bin                                            # Personal bins
-  /Applications/Postgres.app/Contents/Versions/9.3/bin # Postgres.app bins
-  /usr/local/bin                                       # Homebrew bins
 
   $path                                                # -- DEFAULT --
 
-  ./node_modules/.bin                                  # npm local bins
+  # ./node_modules/.bin                                # npm local bins
 
 )
 
 export PATH
-
