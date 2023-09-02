9e475dcc82907d5abfeb1472f7c65b26abab50f4
Tobias Adam <tow8ie@users.noreply.github.com>
2023-09-02 17:46:05 +0200

    Deactivate all rcm hooks.

--- hooks/post-up/main.sh
+++ hooks/post-up/main.sh
@@ -1,4 +1,4 @@
-source "${HOME}/.dotfiles/install-homebrew-formulas.sh"
+# source "${HOME}/.dotfiles/install-homebrew-formulas.sh"
 # source "${HOME}/.dotfiles/configure-iterm.sh"
 # source "${HOME}/.dotfiles/install-emacs-packages.sh"
 # source "${HOME}/.dotfiles/install-vim-bundles.sh"
