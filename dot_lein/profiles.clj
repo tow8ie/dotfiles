20c8b86e2134ab157f9a6391cf9d9ef541137821
Tobias Adam <tow8ie@users.noreply.github.com>
2026-02-16 18:32:09 +0100

    Merge with my work dotfile adjustments.

--- dot_lein/profiles.clj
+++ dot_lein/profiles.clj
@@ -1,2 +1,6 @@
-{:user {:plugins [[lein-pprint "1.2.0"]
-                  [lein-ancient "0.6.15"]]}}
+{:user {:plugins [[lein-ancient "1.0.0-RC3"]
+                  [lein-pprint "1.3.2"]]
+        :dependencies [[org.clojure/tools.reader "1.5.2"]]}
+ :hashp {:dependencies [[dev.weavejester/hashp "0.4.0"]]
+         :injections [(require 'hashp.preload)]}
+ :portal {:dependencies [[djblue/portal "0.59.2"]]}}
