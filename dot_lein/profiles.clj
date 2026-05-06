a0eb4f96feb9b1af7e3bc86224af91928aad5e77
Tobias Adam <tow8ie@users.noreply.github.com>
2026-05-06 17:42:17 +0200

    Remove fixed tools.reader dependency from Leiningen's user profile.

--- dot_lein/profiles.clj
+++ dot_lein/profiles.clj
@@ -1,6 +1,5 @@
 {:user {:plugins [[lein-ancient "1.0.0-RC3"]
-                  [lein-pprint "1.3.2"]]
-        :dependencies [[org.clojure/tools.reader "1.5.2"]]}
+                  [lein-pprint "1.3.2"]]}
  :hashp {:dependencies [[dev.weavejester/hashp "0.4.0"]]
          :injections [(require 'hashp.preload)]}
  :portal {:dependencies [[djblue/portal "0.59.2"]]}}
