73d03465e57993fd36287ddca9e45c966b57d9c1
Tobias Adam <tow8ie@users.noreply.github.com>
2026-07-30 17:10:21 +0200

    Update versions in Leiningen user profile.

--- dot_lein/profiles.clj
+++ dot_lein/profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[lein-ancient "1.0.0-RC3"]
+{:user {:plugins [[lein-ancient "1.0.0"]
                   [lein-pprint "1.3.2"]]}
- :hashp {:dependencies [[dev.weavejester/hashp "0.4.0"]]
+ :hashp {:dependencies [[dev.weavejester/hashp "0.5.1"]]
          :injections [(require 'hashp.preload)]}
- :portal {:dependencies [[djblue/portal "0.59.2"]]}}
+ :portal {:dependencies [[djblue/portal "0.67.1"]]}}
