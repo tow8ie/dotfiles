e8c95aa8274ff4bf58574029639834f13a164693
Tobias Adam <tow8ie@users.noreply.github.com>
2026-07-31 10:46:30 +0200

    Fix hashp integration for Leiningen.

--- dot_lein/profiles.clj
+++ dot_lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "1.0.0"]
                   [lein-pprint "1.3.2"]]}
  :hashp {:dependencies [[dev.weavejester/hashp "0.5.1"]]
-         :injections [(require 'hashp.preload)]}
+         :injections [((requiring-resolve 'hashp.install/install!))]}
  :portal {:dependencies [[djblue/portal "0.67.1"]]}}
